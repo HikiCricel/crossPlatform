a06f820515a1d6438e969a458c3972026d0256d2
Hiki Cricel <daniilfisenko51005@gmail.com>
2025-11-06 16:46:32 +0300

    feat: changes in repository - add @Repository in StudentRepository and TimeEntryRepository - add JpaSpecificationExecutor in StudentRepository and TimeEntryRepository

--- src/main/java/com/example/crossPlatform/repository/StudentRepository.java
+++ src/main/java/com/example/crossPlatform/repository/StudentRepository.java
@@ -3,10 +3,13 @@ package com.example.crossPlatform.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.example.crossPlatform.model.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
         List<Student> findByGroupStartingWithIgnoreCase(String group);
 
         List<Student> findAllByGroup(String group);
--- src/main/java/com/example/crossPlatform/repository/TimeEntryRepository.java
+++ src/main/java/com/example/crossPlatform/repository/TimeEntryRepository.java
@@ -3,10 +3,13 @@ package com.example.crossPlatform.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.example.crossPlatform.model.TimeEntry;
 
-public interface TimeEntryRepository extends JpaRepository<TimeEntry, Long> {
+@Repository
+public interface TimeEntryRepository extends JpaRepository<TimeEntry, Long>, JpaSpecificationExecutor<TimeEntry> {
     List<TimeEntry> findByDescriptionStartingWithIgnoreCase(String description);
 
     List<TimeEntry> findAllByDescription(String description);
