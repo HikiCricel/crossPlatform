f2dc75d19be6e4e001186fc33bd9fac50bbb21c7
Hiki Cricel <daniilfisenko51005@gmail.com>
2025-11-06 17:08:08 +0300

    feat: add spesifications

---
+++ src/main/java/com/example/crossPlatform/specifications/StudentSpecifications.java
@@ -0,0 +1,27 @@
+package com.example.crossPlatform.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.example.crossPlatform.model.Student;
+
+public class StudentSpecifications {
+    private static Specification<Student> nameLike(String name){
+        return (root, query, criteriaBuilder) ->{
+            if(name==null||name.trim().isEmpty()){return null;}
+            return criteriaBuilder.like(criteriaBuilder.lower(
+                root.get("name")), "%" + name.trim().toLowerCase() + "%");
+        };
+    }
+
+    private static Specification<Student> groupLike(String group){
+        return (root, query, criteriaBuilder) ->{
+            if(group==null||group.trim().isEmpty()){return null;}
+            return criteriaBuilder.like(criteriaBuilder.lower(
+                root.get("group")), "%" + group.trim().toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Student> filter (String name, String group) {
+        return Specification.allOf(nameLike(name), groupLike(group));
+    }
+}
---
+++ src/main/java/com/example/crossPlatform/specifications/TimeEntrySpecifications.java
@@ -0,0 +1,64 @@
+package com.example.crossPlatform.specifications;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.example.crossPlatform.enums.TaskType;
+import com.example.crossPlatform.model.Student;
+import com.example.crossPlatform.model.TimeEntry;
+
+public class TimeEntrySpecifications {
+    private static Specification<TimeEntry> studentLike(Student student) {
+        return (root, query, criteriaBuilder) -> {
+            if (student == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("student"), student);
+        };
+    }
+
+    private static Specification<TimeEntry> typeLike(TaskType type) {
+        return (root, query, criteriaBuilder) -> {
+            if (type == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("type"), type);
+        };
+    }
+
+    private static Specification<TimeEntry> timeDateStartLike(LocalDateTime timeStart) {
+        return (root, query, criteriaBuilder) -> {
+            if (timeStart == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("type"), timeStart);
+        };
+    }
+
+    private static Specification<TimeEntry> timeDateEndLike(LocalDateTime timeEnd) {
+        return (root, query, criteriaBuilder) -> {
+            if (timeEnd == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("type"), timeEnd);
+        };
+    }
+
+    private static Specification<TimeEntry> isBillableLike(boolean expr){
+        return (root, query, criteriaBuilder) ->{
+            return criteriaBuilder.equal(root.get("isBillable"), expr);
+        };
+    }
+
+    public static Specification<TimeEntry> filter(TaskType type, Student student,
+    LocalDateTime dateTimeStart, LocalDateTime dateTimeEnd, boolean expression){
+        return Specification.allOf(
+            typeLike(type),
+            studentLike(student),
+            timeDateStartLike(dateTimeStart),
+            timeDateEndLike(dateTimeEnd),
+            isBillableLike(expression)
+        );
+    }
+}
