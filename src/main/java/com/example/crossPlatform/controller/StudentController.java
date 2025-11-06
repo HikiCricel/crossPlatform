edc8c8f3586b342f906a92fef5f276ea1b9cea72
Hiki Cricel <daniilfisenko51005@gmail.com>
2025-11-06 16:43:04 +0300

    feat: new methods in controllers - add addTimEntity, getTimeEntry, deleteTimEntity in TimeEntryController - add getByFilter method in StudentController and TimeEntryController

--- src/main/java/com/example/crossPlatform/controller/StudentController.java
+++ src/main/java/com/example/crossPlatform/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.crossPlatform.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,8 +21,6 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
@@ -31,19 +31,19 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    
     @PostMapping("/students")
     public ResponseEntity<Student> addStudent(@RequestBody @Valid Student student) {
         Student newStudent = studentService.create(student);
         return ResponseEntity.status(HttpStatus.CREATED).body(newStudent);
     }
-    
+
     @GetMapping
     public List<Student> getStudents(@RequestParam(required = false) String name) {
-        if(name == null) return studentService.getAll();
-        else return studentService.getAllByGroup(name);
+        if (name == null)
+            return studentService.getAll();
+        else
+            return studentService.getAllByGroup(name);
     }
-    
 
     @GetMapping("/students/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable Long id) {
@@ -55,17 +55,24 @@ public class StudentController {
         Student updated = studentService.update(id, student);
         if (updated != null) {
             return ResponseEntity.ok(updated);
-        }
-        else {
+        } else {
             return ResponseEntity.notFound().build();
-        }        
+        }
     }
 
     @DeleteMapping("/students/{id}")
-    public ResponseEntity <Void> deleteStudent(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
         if (studentService.deleteById(id)) {
             ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok().build();
-    } 
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<Object> getByFilter(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String title,
+            @PageableDefault(page = 0, size = 10, sort = "title") Pageable pageable) {
+        return ResponseEntity.ok(studentService.getByFilter(name, title, pageable));
+    }
 }
--- src/main/java/com/example/crossPlatform/controller/TimeEntryController.java
+++ src/main/java/com/example/crossPlatform/controller/TimeEntryController.java
@@ -1,53 +1,64 @@
 package com.example.crossPlatform.controller;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.crossPlatform.enums.TaskType;
 import com.example.crossPlatform.model.Student;
 import com.example.crossPlatform.model.TimeEntry;
+import com.example.crossPlatform.service.TimeEntryService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/timeEntries")
 public class TimeEntryController {
-    private List<TimeEntry> timeEntries = new ArrayList<>(Arrays.asList(
-    new TimeEntry(1l, new Student(1l, "Фисенко Д.Р.", "2231121", null), TaskType.CODING , "Кусовая работа", LocalDateTime.now(), LocalDateTime.now(), false),
-    new TimeEntry(2l, new Student(2l, "Зубков Е.В.", "2201117", null), TaskType.EXAM_PREP, "Экзамен", LocalDateTime.now(), LocalDateTime.now(), true)
-    ));
+    private final TimeEntryService timeEntryService;
 
-    @GetMapping
-    public List<TimeEntry> getTimeEntries() {
-        return timeEntries;
+    public TimeEntryController(TimeEntryService timeEntryService) {
+        this.timeEntryService = timeEntryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TimeEntry> addTimEntity(@RequestBody @Valid TimeEntry timeEntry) {
+        TimeEntry newTimeEntry = timeEntryService.create(timeEntry);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTimeEntry);
     }
 
     @GetMapping("/timeEntries/{id}")
     public ResponseEntity<TimeEntry> getTimeEntry(@PathVariable Long id) {
-        for (TimeEntry timeEntry : timeEntries) {
-            if (timeEntry.getId().equals(id)) {
-                return ResponseEntity.ok(timeEntry);
-            }
-        }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(timeEntryService.getById(id));
     }
 
-    @PostMapping("/timeEntries")
-    public ResponseEntity<TimeEntry> addTimeEntry(@RequestBody @Valid TimeEntry timeEntry) {
-        timeEntry.setId((long)timeEntries.size() + 1);
-        timeEntries.add(timeEntry);
-        return ResponseEntity.status(HttpStatus.CREATED).body(timeEntry);
+    @GetMapping("/filter")
+    public ResponseEntity<Object> getByFilter(
+        @RequestParam(required = false)Student student,
+        @RequestParam(required = false)TaskType type,
+        @RequestParam(required = false)LocalDateTime dateTimeStart,
+        @RequestParam(required = false)LocalDateTime dateTimeEnd,
+        @RequestParam(required = false)boolean expression,
+        @PageableDefault(page = 0, size = 10, sort = "title")Pageable pageable){
+            return ResponseEntity.ok(timeEntryService.getByFilter(student, type, dateTimeStart, dateTimeEnd, expression, pageable));
+    }
+
+    @DeleteMapping("/timeEntries/{id}")
+    public ResponseEntity<Void> deleteTimEntity(@PathVariable Long id){
+        if(timeEntryService.deleteById(id)){
+            ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
 
